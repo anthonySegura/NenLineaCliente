afbbc34463d8756734439949a4030f2e8ebe8396
Anthony Segura <ansechaves@hotmail.com>
2017-08-27 12:28:31 -0600

    Vista de inicio de sesión

--- src/App.js
+++ src/App.js
@@ -2,20 +2,57 @@ import React, { Component } from 'react';
 import NavBar from './components/navbar/NavBar.js';
 import MiddleBox from './components/middleContent/MiddleBox.js';
 import SessionsArea from './components/sessionsList/SessionsArea.js';
+import Welcome from './components/welcomePage/Welcome.js';
 
 class App extends Component {
 
-	renderBody(){
+	constructor(){
+		super();
+		this.state = {
+			playing: false,
+			logged: false
+		}
+		this.renderBody = this.renderBody.bind(this);
+		this.login = this.login.bind(this);
+	}
 
+	login(){
+		this.setState({
+			logged: true
+		})
+	}
+
+	renderBody(){
+		// Se renderiza la vista principal
+		if(this.state.logged){
+			if(!this.state.playing){
+				return(
+					<div className="App">
+						<NavBar/>
+						<MiddleBox/>
+						<SessionsArea/>
+					</div>
+				)
+			}
+			// Se renderiza la vista de partida
+			else {
+				return(
+					<div>
+
+					</div>
+				)
+			}
+		}
+		else{
+			return(
+				<Welcome login = {this.login}/>
+			)
+		}
 	}
 
   render() {
     return (
-      <div className="App">
-				<NavBar/>
-	      <MiddleBox/>
-	      <SessionsArea/>
-      </div>
+			this.renderBody()
     );
   }
 }
---
+++ src/components/welcomePage/Welcome.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import GoogleLogin from 'react-google-login';
+import background from './background.png';
+
+class Welcome extends Component{
+
+	success(response){
+		this.props.login();
+	}
+
+	error(response){
+
+	}
+
+	loading(response){
+
+	}
+
+	render(){
+
+		const backgroundStyle = {
+			width: "100%",
+			height: "100%",
+			position: "fixed",
+			left: "0px",
+			top: "0px",
+			zIndex: "-1"
+		}
+
+		const stretch = {
+			width: "100%",
+      height: "100%"
+		}
+
+		const centered = {
+			position: "absolute",
+			top: "50%",
+			left: "50%",
+			transform: "translate(-50%, -50%)"
+		}
+
+		const textStyle = {
+			color: "white",
+			paddingBottom: "2em"
+		}
+
+		return(
+			<div style= {backgroundStyle}>
+				<img src= {background} style={stretch}/>
+				<div style= {centered}>
+					<h2 className="text-center">N En Linea</h2>
+
+					<GoogleLogin
+						clientId = "730064414140-pddt1udno397off9gdmj3so2teom4fv8.apps.googleusercontent.com"
+						onSuccess = {this.success.bind(this)}
+						onFailure = {this.error}
+						onRequest = {this.loading}
+						approvalPrompt="force"
+						responseType="code"
+						isSignedIn={true}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Welcome;
